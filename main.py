5aafb8de8026354866f3612df169adbc70b4a677
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-06 00:20:22 +0300

    record audio function returns voice data

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ recognizer = sr.Recognizer() # responsible for recognizing speech
 def record_audio():
     with sr.Microphone() as source: # the microphone will be our source
         audio = recognizer.listen(source)
+        voice_data = ''
         try:
             voice_data = recognizer.recognize_google(audio)
         except sr.UnknownValueError:
             print("Sorry, I did not get that")
         except sr.RequestError:
             print("Sorry, my speech service is down at the moment")
+        return voice_data
 
 print("How can I assist you?")
 voice_data = record_audio()
