5893a53ab4123a88446333f6a2f6deeeffd3eb56
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-06 00:11:03 +0300

    handle unrecognized audio and request errors

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer() # responsible for recognizing speech
 
-with sr.Microphone() as source:
+with sr.Microphone() as source: # the microphone will be our source
     print("Say something...")
     audio = recognizer.listen(source)
-    voice_data = recognizer.recognize_google(audio)
-    print(voice_data)
+    try:
+        voice_data = recognizer.recognize_google(audio)
+        print(voice_data)
+    except sr.UnknownValueError:
+        print("Sorry, I did not get that")
+    except sr.RequestError:
+        print("Sorry, my speech service is down at the moment")
 
