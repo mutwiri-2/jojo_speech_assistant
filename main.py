6fc6f37382589c70285d610ba87299961014b51b
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-06 00:12:58 +0300

    create a function to record audio

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer() # responsible for recognizing speech
 
-with sr.Microphone() as source: # the microphone will be our source
-    print("Say something...")
-    audio = recognizer.listen(source)
-    try:
-        voice_data = recognizer.recognize_google(audio)
-        print(voice_data)
-    except sr.UnknownValueError:
-        print("Sorry, I did not get that")
-    except sr.RequestError:
-        print("Sorry, my speech service is down at the moment")
+def record_audio:
+    with sr.Microphone() as source: # the microphone will be our source
+        print("Say something...")
+        audio = recognizer.listen(source)
+        try:
+            voice_data = recognizer.recognize_google(audio)
+            print(voice_data)
+        except sr.UnknownValueError:
+            print("Sorry, I did not get that")
+        except sr.RequestError:
+            print("Sorry, my speech service is down at the moment")
 
