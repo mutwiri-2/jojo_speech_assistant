d02149c7247a81d251bdc8f22481b6da5b17f666
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-06 01:40:54 +0300

    handle alsa errors from pyaudio

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import speech_recognition as sr
+import alsa_error_handler
 
 recognizer = sr.Recognizer() # responsible for recognizing speech
 
 def record_audio():
-    with sr.Microphone() as source: # the microphone will be our source
-        audio = recognizer.listen(source)
-        voice_data = ''
-        try:
-            voice_data = recognizer.recognize_google(audio)
-        except sr.UnknownValueError:
-            print("Sorry, I did not get that")
-        except sr.RequestError:
-            print("Sorry, my speech service is down at the moment")
-        return voice_data
+    with alsa_error_handler.noalsaerr():
+        with sr.Microphone() as source: # the microphone will be our source
+            audio = recognizer.listen(source)
+            voice_data = ''
+            try:
+                voice_data = recognizer.recognize_google(audio)
+            except sr.UnknownValueError:
+                print("Sorry, I did not get that")
+            except sr.RequestError:
+                print("Sorry, my speech service is down at the moment")
+            return voice_data
 
 print("How can I assist you?")
 voice_data = record_audio()
