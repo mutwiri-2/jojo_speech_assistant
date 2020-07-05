20a3f275074db1c39bfb97b37ba14920543bdbb5
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-06 00:15:00 +0300

    refactor record audio function

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ recognizer = sr.Recognizer() # responsible for recognizing speech
 
 def record_audio:
     with sr.Microphone() as source: # the microphone will be our source
-        print("Say something...")
         audio = recognizer.listen(source)
         try:
             voice_data = recognizer.recognize_google(audio)
-            print(voice_data)
         except sr.UnknownValueError:
             print("Sorry, I did not get that")
         except sr.RequestError:
