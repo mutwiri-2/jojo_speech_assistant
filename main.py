cec65eee57c59925998ae25fd1288f5e4876c14c
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-06 03:23:41 +0300

    use the speech assistant speak function

--- main.py
+++ main.py
@@ -16,50 +16,50 @@ def record_audio(ask=False):
     with alsa_error_handler.noalsaerr():
         with sr.Microphone() as source: # the microphone will be our source
             if ask:
-                print(ask)
+                jojo_speak(ask)
             audio = recognizer.listen(source)
             voice_data = ''
             try:
                 voice_data = recognizer.recognize_google(audio)
             except sr.UnknownValueError:
-                print("Sorry, I did not get that")
+                jojo_speak("Sorry, I did not get that")
             except sr.RequestError:
-                print("Sorry, my speech service is down at the moment")
+                jojo_speak("Sorry, my speech service is down at the moment")
             return voice_data
 
 def respond(voice_data):
     # get name
     if 'what is your name' in voice_data:
-        print('My name is jojo')
+        jojo_speak('My name is jojo')
     # get today's date
     today = date.today()
     date_today = today.strftime("%B %d, %Y")
     if 'what date is it' in voice_data:
-        print('Today\'s date is ' + date_today)
+        jojo_speak('Today\'s date is ' + date_today)
     
     # get the current time
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     if 'what is the time' in voice_data:
-        print('The time is ' + current_time)
+        jojo_speak('The time is ' + current_time)
 
     # search 
     if 'search' in voice_data:
         search_term = record_audio('What do you want to search for?')
         url = 'https://google.com/search?q=' + search_term
         webbrowser.get().open(url)
-        print("Here is what I found for " + search_term)
+        jojo_speak("Here is what I found for " + search_term)
 
     # find a place on Google maps
     if 'find location' in voice_data:
         location = record_audio('What is the location?')
         url = 'https://google.nl/maps/place/' + location + '/&amp;'
         webbrowser.get().open(url)
-        print('Here is the location of ' + location)
+        jojo_speak('Here is the location of ' + location)
     
     # exit
     if 'quit' in voice_data:
-        print("Goodbye")
+        jojo_speak("Goodbye")
         exit()
 
 def jojo_speak(audio_string):
@@ -72,7 +72,7 @@ def jojo_speak(audio_string):
     os.remove(audio_file)
 
 time.sleep(1)
-print("How can I assist you?")
+jojo_speak("How can I assist you?")
 while True:
     voice_data = record_audio()
     respond(voice_data)
